106eaa377043e07d1b08cf6c9253960550e16879
Mathisrrr <mathisrivallan5@gmail.com>
2024-10-24 12:06:58 +0200

    neww

--- dev/Interface graphique.py
+++ dev/Interface graphique.py
@@ -27,13 +27,6 @@ g.dessinerRectangle(20,120,460,80,"grey")
 
 g.dessinerRectangle(240,220,200,80,"grey" )
 
-"""""""""""
-plus=g.afficherTexte("+",100,345,"black",60)
-moins=g.afficherTexte("-",200,345,"black",60)
-multiplier=g.afficherTexte("x",300,345,"black",60)
-diviser=g.afficherTexte("/",400,350,"black",60)
-egal=g.afficherTexte("=",120,250,"black",150)
-"""""
 g.attendreClic()
 g.fermerFenetre()
 
